5b9c2fa0e415eb919ba57aadd6d90e4f4b481ecd
Jorawer Grewal <joegrewal20@gmail.com>
2022-03-04 22:49:09 -0800

    Add Empty and Show stories and passed props to Appointment Form

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,4 +1,7 @@
-import React from 'react';
+import React, { Fragment } from 'react'
+import Header from './Header';
+import Show from './Show';
+import Empty from './Empty';
 
 import "components/Appointment/styles.scss"
 
@@ -7,7 +10,8 @@ export default function Appointment(props) {
   return (
     <article className="appointment">
       <>
-        {props.time ? `Appointment at ${props.time}` : "No Appointments"}
+        <Header time={props.time}/>
+        {props.interview ? <Show student={props.interview.student} interviewer={props.interview.interviewer} /> : <Empty />}
       </>
     </article>
   )
