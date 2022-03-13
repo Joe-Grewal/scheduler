8476c0a2b8bf0136cd65052feaf9a4ebcd324fc8
Jorawer Grewal <joegrewal20@gmail.com>
2022-03-13 15:42:42 -0700

    Implement getInterview function to transform interviewer data

--- src/components/Application.js
+++ src/components/Application.js
@@ -5,34 +5,41 @@ import "components/Application.scss";
 
 import DayList from "./DayList";
 import Appointment from "components/Appointment";
-import { getAppointmentsForDay } from "helpers/selectors";
+import { getAppointmentsForDay, getInterview } from "helpers/selectors";
+import InterviewerList from "./InterviewerList";
 
 export default function Application(props) {
   const [state, setState] = useState({
     day: "Monday",
     days: [],
-    appointments: {}
+    appointments: {},
+    interviewers: {}
   });
 
-  const dailyAppointments = getAppointmentsForDay(state, state.day)
+  const appointments = getAppointmentsForDay(state, state.day)
 
   const setDay = day => setState({ ...state, day });
 
   useEffect(() => {
     Promise.all([
       axios.get('/api/days'),
-      axios.get('/api/appointments')
+      axios.get('/api/appointments'),
+      axios.get('/api/interviewers'),
     ]).then((all) => {
       console.log(all);
-      setState(prev => ({...prev, days: all[0].data, appointments: all[1].data }));
+      setState(prev => ({...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data }));
     })
   }, [])
 
-  const schedule = Object.values(dailyAppointments).map(appointment => {
+  const schedule = appointments.map(appointment => {
+    const interview = getInterview(state, appointment.interview);
+
     return (
       <Appointment 
-        key={appointment.id} 
-        {...appointment} 
+        key={appointment.id}
+        id={appointment.id}
+        time={appointment.time}
+        interview={interview}
       />
     );
   }); 
--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -5,4 +5,15 @@ export function getAppointmentsForDay(state, day) {
   } else {
     return filteredDay[0].appointments.map(id => state.appointments[id]);
   }
-}
+};
+
+export function getInterview(state, interview) {
+  if (!interview) {
+    return null;
+  } else {
+    return {
+      student: interview.student,
+      interviewer: state.interviewers[interview.interviewer]
+    } 
+  }
+};
