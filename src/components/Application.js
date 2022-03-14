eb9b8d445da0107311a30c3c5c429ae6301ec2a5
Jorawer Grewal <joegrewal20@gmail.com>
2022-03-13 21:23:55 -0700

    Add and implement bookInterview and save functions

--- src/components/Application.js
+++ src/components/Application.js
@@ -21,6 +21,25 @@ export default function Application(props) {
 
   const setDay = day => setState({ ...state, day });
 
+  function bookInterview(id, interview) {
+    console.log(id, interview);
+
+    const appointment = {
+      ...state.appointments[id],
+      interview: { ...interview }
+    };
+
+    const appointments = {
+      ...state.appointments,
+      [id]: appointment
+    };
+    
+    setState({
+      ...state,
+      appointments
+    });
+  }
+
   useEffect(() => {
     Promise.all([
       axios.get('/api/days'),
@@ -42,6 +61,7 @@ export default function Application(props) {
         time={appointment.time}
         interview={interview}
         interviewers={interviewers}
+        bookInterview={bookInterview}
       />
     );
   }); 
